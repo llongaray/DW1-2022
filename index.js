e2fbda055bdd4b0bf5a9ecaa8fba5b8b8a648b17
llongaray <llongaray77@gmail.com>
2022-12-05 20:48:03 -0300

    add

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+function openProjects(){
+  document.getElementById('project').classList.replace('desativado', 'ativado');
+  document.getElementById('abas-screen').classList.replace('desativado', 'ativado');
+}
+function openReadme(){
+  document.getElementById('readme').classList.replace('desativado', 'ativado');
+  document.getElementById('abas-screen').classList.replace('desativado', 'ativado');
+}
+function openContacts(){
+  document.getElementById('contact').classList.replace('desativado', 'ativado');
+  document.getElementById('abas-screen').classList.replace('desativado', 'ativado');
+}
+function exitProjects(){
+  document.getElementById('project').classList.replace('ativado', 'desativado');
+  document.getElementById('abas-screen').classList.replace('ativado', 'desativado');
+}
+function exitReadme(){
+  document.getElementById('readme').classList.replace('ativado', 'desativado');
+  document.getElementById('abas-screen').classList.replace('ativado', 'desativado');
+}
+function exitContact(){
+  document.getElementById('contact').classList.replace('ativado', 'desativado');
+  document.getElementById('abas-screen').classList.replace('ativado', 'desativado');
+}
